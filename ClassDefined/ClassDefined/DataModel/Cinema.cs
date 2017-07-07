08544ae5b9f23c039f7104609f19a3c1d62b2d50
Andrei Profir <andrei.profir@outlook.com>
2017-07-07 17:25:46 +0300

    In clasa Cinema au fost modificate proprietatile(din simple in AutoPropery)

--- ClassDefined/ClassDefined/DataModel/Cinema.cs
+++ ClassDefined/ClassDefined/DataModel/Cinema.cs
@@ -7,97 +7,31 @@ namespace ClassDefined.DataModel
 {
     public class Cinema
     {
-        public long Id
-        {
-            get => default(int);
-            set
-            {
-            }
-        }
+        public long Id { get; set; }
 
-        public string ShortName
-        {
-            get => default(string);
-            set
-            {
-            }
-        }
+        public string ShortName { get; set; }
 
-        public string LongName
-        {
-            get => default(string);
-            set
-            {
-            }
-        }
+        public string LongName { get; set; }
 
-        public string Address
-        {
-            get => default(string);
-            set
-            {
-            }
-        }
+        public string Address { get; set; }
 
-        public List<string> ContactPhones
-        {
-            get => new List<string>();
-            set
-            {
-            }
-        }
+        public List<string> ContactPhones { get; set; }
 
-        public string City
-        {
-            get => default(string);
-            set
-            {
-            }
-        }
+        public string City { get; set; }
 
-        public string Info
-        {
-            get => default(string);
-            set
-            {
-            }
-        }
+        public string Info { get; set; }
 
-        public int? Likes
-        {
-            get => default(int);
-            set
-            {
-            }
-        }
+        public int? Likes { get; set; }
 
-        public int? Dislikes
-        {
-            get => default(int);
-            set
-            {
-            }
-        }
+        public int? Dislikes { get; set; }
 
-        public List<string> Photos
-        {
-            get => new List<string>();
-            set
-            {
-            }
-        }
+        public List<string> Photos { get; set; }
 
         public List<Hall> Halls
         {
             get; set;
         }
 
-        public List<Comment> Reviews
-        {
-            get => default(int);
-            set
-            {
-            }
-        }
+        public List<Comment> Reviews { get; set; }
     }
 }
