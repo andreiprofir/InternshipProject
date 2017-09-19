020d69548000e51447cca1556e79f17ce08a4ca4
Andrei Profir <profir.andrei94@gmail.com>
2017-09-20 00:00:02 +0300

    Am adaugat posibilitatea de a vedea trailer-ul pentru un film la apasarea butonului de sub poster

--- CinemaTickets/src/CinemaTickets.Web/wwwroot/js/site.js
+++ CinemaTickets/src/CinemaTickets.Web/wwwroot/js/site.js
@@ -1,1 +1,28 @@
-﻿// Write your JavaScript code.
+﻿
+//script for modal window for display movie trailer
+$(function () {
+    $("#trailer-modal").on("show.bs.modal",
+        function() {
+            var src = $("#videowrapper").attr("src");
+
+            var index = src.indexOf("?");
+
+            src = src.substring(0, index);
+
+            src += "?autoplay=1";
+
+            $("#videowrapper").attr("src", src);
+        });
+    $("#trailer-modal").on("hide.bs.modal",
+        function() {
+            var src = $("#videowrapper").attr("src");
+
+            var index = src.indexOf("?");
+
+            src = src.substring(0, index + 1);
+
+            $("#videowrapper").attr("src", "");
+
+            $("#videowrapper").attr("src", src);
+        });
+});
